796c2160ce8294106704354cc6b419af18684a76
RuffRuff7 <49106053+RuffRuff7@users.noreply.github.com>
2019-11-16 13:15:32 -0800

    Update scripts.js

--- slack/public/scripts.js
+++ slack/public/scripts.js
@@ -1,10 +1,4 @@
     const socket = io('http://localhost:9000');
-
-
-    console.log(socket.io)
-    socket.on('connect',()=>{
-        console.log(socket.id);
-    })
     
     socket.on('nsList',(nsData)=>{
         console.log("The list of namespaces has arrived")
@@ -22,22 +16,5 @@
                 console.log(`${nsEndpoint} I should go to now`)
             })
         })
-
+        joinNs('/wiki');
     })
-
-    socket.on('messageFromServer',(dataFromServer)=>{
-        console.log(dataFromServer);
-        socket.emit('dataToServer',{data: "Data from the Client!"})
-    })
-
-    document.querySelector('#message-form').addEventListener('submit',(event)=>{
-        event.preventDefault();
-        const newMessage = document.querySelector('#user-message').value;
-        socket.emit('newMessageToServer',{text: newMessage})
-    })
-
-    sockey.on('messageToClients',(msg)=> {
-        console.log(msg);
-        document.querySelector('#messages').innerHTML += `<li>${msg.text}</li>`
-    })
-
