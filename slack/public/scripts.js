17c54d599186450c5943964477c9650eb4ff7b71
RuffRuff7 <49106053+RuffRuff7@users.noreply.github.com>
2019-12-07 12:31:23 -0800

    Finished Project

--- slack/public/scripts.js
+++ slack/public/scripts.js
@@ -1,20 +1,29 @@
-    const socket = io('http://localhost:9000');
-    
-    socket.on('nsList',(nsData)=>{
-        console.log("The list of namespaces has arrived")
-        // console.log(nsData)
-        let namespacesDiv = document.querySelector('.namespaces');
-        namespacesDiv.innerHTML = "";
-        nsData.forEach((ns)=>{
-            namespacesDiv.innerHTML += `<div class ="namespace" ns=${ns.endpoint} ><img src="${ns.img}" /></div>`
-        })
-        console.log(document.getElementsByClassName('namespace'))
-        Array.from(document.getElementsByClassName('namespace')),forEach((elem)=>{
-            // console.log(elem)
-            elem.addEventListener('click',(e)=>{
-                const nsEndpoint = elem.getAttribute('ns');
-                console.log(`${nsEndpoint} I should go to now`)
-            })
-        })
-        joinNs('/wiki');
+const username = prompt("What is your username")
+// const socket = io('http://localhost:9000');
+const socket = io('http://localhost:9000',{
+    query: {
+        username,
+
+    }
+});
+let nsSocket = "";    
+
+socket.on('nsList',(nsData)=>{
+    console.log("The list of namespaces has arrived")
+    // console.log(nsData)
+    let namespacesDiv = document.querySelector('.namespaces');
+    namespacesDiv.innerHTML = "";
+    nsData.forEach((ns)=>{
+        namespacesDiv.innerHTML += `<div class ="namespace" ns=${ns.endpoint} ><img src="${ns.img}" /></div>`
     })
+    console.log(document.getElementsByClassName('namespace'))
+    Array.from(document.getElementsByClassName('namespace')),forEach((elem)=>{
+        // console.log(elem)
+        elem.addEventListener('click',(e)=>{
+            const nsEndpoint = elem.getAttribute('ns');
+            console.log(`${nsEndpoint} I should go to now`)
+            joinNs(nsEndpoint);
+        })
+    })
+    joinNs('/wiki');
+})
